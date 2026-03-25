058c6ffb798d727cfc7b7fc3a2b8341670c4acd1
Edisson granados <edissongranados@gmail.com>
2026-03-24 19:35:47 -0500

    Add files via upload

---
+++ disponibilizacion/seguridad.py
@@ -0,0 +1,10 @@
+# --- SISTEMA DE CONTROL DE USUARIOS (LOGIN) ---
+# Aquí puedes agregar o eliminar clientes. Formato: "usuario": "contraseña"
+USUARIOS_AUTORIZADOS = {
+    "Diana": "Oriente2026",
+    "Thalia": "Bello2026",
+    "Catalina": "itagui2026",
+    "Sara": "Envigado2026",
+    "Sgerencia": "2026",
+    "Ggerencia": "2026"
+}
